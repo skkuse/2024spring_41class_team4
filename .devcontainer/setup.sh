3146e1ba2de4d111cc9822f0f207a16205b5b905
goo314 <rn0731@g.skku.edu>
2024-05-29 13:35:27 +0000

    chore(be): change db config and connect it to api

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,8 +2,7 @@
 set -x
 
 # Install Python packages
-python3 -m pip install -r /workspace/backend/requirements.txt
+python3 -m pip install -r /workspace/api/requirements.txt
 
 # Django Setup
 # python3 /workspace/backend/manage.py migrate
-# python3 /workspace/backend/manage.py inituser --username root --password rootroot --action create_super_admin
--- api/api/settings.py
+++ api/api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        'NAME': "db",
+        'USER' : "user",
+        'PASSWORD' : "1234",
+        "HOST" : "127.0.0.1",
+        "PORT" : "3309",
     }
 }
 
